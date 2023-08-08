c2b8d2db2404ae85527e0f769e997c01d49d2828
forlocks <sheluhen@mail.ru>
2023-08-08 14:41:48 +0300

    init: start reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+  let result = '';
+
+  n = n.toString();
+
+  for (let i = n.length - 1; i >= 0; i--) {
+    if (n[i] === '-') {
+      result = result;
+    } else {
+      result += n[i];
+    }
+  }
+
+  return +result;
 }
